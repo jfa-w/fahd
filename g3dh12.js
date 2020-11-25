e7615cd9f0666b4ac07d2c24037c7b4809a911f5
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-11-25 15:03:37 +0300

    Update g3dh12.js

--- g3dh12.js
+++ g3dh12.js
@@ -8,21 +8,13 @@ $('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '1px solid #E
 
 
 $(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
-<marquee direction="right" width="65%" " id="jfa-w" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  |آهلا وسهلا بكم في شات عسيرية دوت كوم ● · نورت وطنك هنا نورتنا ·● مع تمنياتنا للجميع بأطيب الاوقات  </marquee></font></b></td>
+<marquee direction="right" width="65%" " id="jfa-w" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  |آهلا وسهلا بكم في شات سهر الخليج ● · نورت وطنك هنا نورتنا ·● مع تمنياتنا للجميع بأطيب الاوقات  </marquee></font></b></td>
 `).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
 $('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid #EDEDED',borderLeft: '3px solid #EDEDED',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
 $('.fr.borderg a').text('دوك موبايل')
 
-$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 0px; width: 0px;">');
-$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="https://www.sudfachat.com/sico/1580237645774.png" width="70%" style="">`)
-$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` <a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;|ألأستماع لـ اشات عسيريه دوت بث مباشر</a><iframe src="https://reach-audio.esteam.rocks/public/nogoum_fm/embed" style="width:100%; height:100px;" scrolling="no" frameborder="no"></iframe></div>
-<center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});
-if(location.protocol!='https:') {location.protocol='https:';}
-if (getUrlParameter('enter') != null) {location.href='/';} 
-AddMsg_=AddMsg;
-AddMsg=function(wid,data){data.msg=data.msg.replace(/\u102C/g,"");return AddMsg_(wid,data);}
 
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('G d=["\\C\\i\\c\\C\\H","\\l\\c\\8\\m\\7\\I\\J","\\i\\j\\K","\\L\\6\\n\\e\\9\\f\\a\\M\\N\\6\\e\\o\\6\\p\\n\\a\\6\\9\\a\\9\\a\\r\\f\\s\\6\\O\\p\\f\\t\\6\\D\\q\\f\\r\\6\\P\\6\\Q\\9\\k\\t\\n\\6\\9\\9\\a\\k\\k\\k\\o\\q\\u\\e\\6\\p\\t\\a\\6\\9\\o\\6\\q\\R\\s\\r\\6\\D\\a\\f\\u\\e\\6\\9\\a\\k\\o\\q\\u\\e\\6\\f\\n\\e\\6\\9\\a\\p\\f\\9\\e\\s\\6","\\j\\8","\\6\\v\\c\\w\\x\\i\\S\\6\\b\\m\\7\\7\\j\\8\\g\\b\\7\\8\\g\\b\\7\\8\\y\\z\\h\\c\\l\\A\\h\\B\\6\\E\\v\\c\\w\\x\\i\\T\\6\\b\\m\\7\\7\\j\\8\\g\\b\\7\\8\\g\\b\\7\\8\\y\\z\\h\\c\\l\\A\\h\\B\\6\\E\\6\\v\\c\\w\\x\\i\\U\\6\\b\\m\\7\\7\\j\\8\\g\\b\\7\\8\\g\\b\\7\\8\\y\\z\\h\\c\\l\\A\\h\\B"];$(d[5])[d[4]](d[0],F(){V(F(){W[d[2]](d[1]);X(d[3])},Y)})',61,61,'||||||x20|x74|x6E|u0627|u0644|x62|x69|_0x3fe7|u0645|u064A|x2E|x72|x6C|x6F|u0633|x6D|x75|u0648|u0646|u0642|u062A|u062F|u0647|u0628|u0643|x64|x76|x23|x2D|x70|x61|x79|x63|u0639|x2C|function|var|x6B|x65|x73|x67|x5B|u0641|u0638|u0631|x5D|u062D|u0634|x31|x32|x33|setTimeout|console|alert|1000'.split('|'),0,{}))
+
 
 													}
 														catch (e){
