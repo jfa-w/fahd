4d693a80050d35e8018a39742225c3cb572465cb
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-11-25 15:06:51 +0300

    Create ch1tt.js

---
+++ ch1tt.js
@@ -0,0 +1,23 @@
+
+try{
+$('.u-msg.dots').removeClass('dots');
+$(".ubnr").remove();
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/jfa-w/fahd/ccfd33a495b9425200fbac1380bdb14166db3c4c/fahad2.css"></style>`).insertBefore("body");
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '1px solid #EDEDED',borderLeft: '1px solid #adadad',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
+
+
+
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" " id="jfa-w" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  |آهلا وسهلا بكم في شات سهر الخليج ● · نورت وطنك هنا نورتنا ·● مع تمنياتنا للجميع بأطيب الاوقات  </marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid #EDEDED',borderLeft: '3px solid #EDEDED',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
+$('.fr.borderg a').text('دوك موبايل')
+
+
+
+
+													}
+														catch (e){
+															console.log(e)
+														}
+
